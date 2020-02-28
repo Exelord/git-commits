cb638d7b1febea9c9387aec70c209caf37a49ea0
Roberto Entringer <r.entringer@gmail.com>
2020-02-28 17:51:35 +0100

    Show collapse all action

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const gitCommitsProvider = new GitCommitsProvider(gitManager);
 
 	context.subscriptions.push(
-		vscode.window.createTreeView('gitCommits', { treeDataProvider: gitCommitsProvider }),
+		vscode.window.createTreeView('gitCommits', { treeDataProvider: gitCommitsProvider, showCollapseAll: true }),
 		vscode.commands.registerCommand('gitCommits.diff', gitManager.compareCommitFileAgainstPrevious)
 	);
 }
