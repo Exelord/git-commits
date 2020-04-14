768da0af4cc2306c2bb4af082fc12e3723f08fd3
Maciej Kwaśniak <kmaciek17@gmail.com>
2020-04-14 20:16:35 +0200

    Fixes and previews

--- src/extension.ts
+++ src/extension.ts
@@ -46,35 +46,37 @@ export function activate(context: vscode.ExtensionContext) {
 			await vscode.env.clipboard.writeText(item.relPath);
 		}),
 
-		vscode.commands.registerCommand('gitCommits.openFile', async (item: ChangeNode) => {
+		vscode.commands.registerCommand('gitCommits.openCurrentFile', async (item: ChangeNode) => {
 			const fileExist = fs.existsSync(item.change.uri.fsPath);
 			if (!fileExist) { return vscode.window.showErrorMessage('This file does not exist anymore'); }
-			await vscode.commands.executeCommand('vscode.open', vscode.Uri.file(item.change.uri.fsPath));
+			await vscode.commands.executeCommand('vscode.open', vscode.Uri.file(item.change.uri.fsPath), { preview: true, viewColumn: vscode.ViewColumn.Active });
+		}),
+
+		vscode.commands.registerCommand('gitCommits.previewFile', async (item: ChangeNode) => {
+			await vscode.commands.executeCommand('vscode.open', item.change.uri, { preview: true, viewColumn: vscode.ViewColumn.Active });
 		}),
 
 		vscode.commands.registerCommand('gitCommits.revertChange', async (item: ChangeNode) => {
 			const result = await vscode.window.showInformationMessage("Are you sure you want to revert these changes?", { modal: true }, { title: 'Revert changes' });
+			if (!result) { return false; }
+		
+			let command;
 
-			
-			if (result) {
-				let command;
-
-				if (item.change.status === Status.INDEX_ADDED) {
-					const fileExist = fs.existsSync(item.change.uri.fsPath);
-					
-					if (fileExist) {
-						command = `rm '${item.relPath}' && git add '${item.relPath}'`;	
-					} else {
-						await vscode.window.showInformationMessage('This change is already reverted');
-					}
+			if (item.change.status === Status.INDEX_ADDED) {
+				const fileExist = fs.existsSync(item.change.uri.fsPath);
+				
+				if (fileExist) {
+					command = `rm '${item.relPath}' && git add '${item.relPath}'`;	
 				} else {
-					command = `git checkout ${item.change.commit.parentHash} -- '${item.originalRelPath}'`;
+					await vscode.window.showInformationMessage('This change is already reverted');
 				}
+			} else {
+				command = `git checkout ${item.change.commit.parentHash} -- '${item.originalRelPath}'`;
+			}
 
-				if (command) {
-					childProcess.execSync(command, { cwd: item.change.commit.repository.rootUri.fsPath });
-					await vscode.window.showInformationMessage('Change has been reverted');
-				}
+			if (command) {
+				childProcess.execSync(command, { cwd: item.change.commit.repository.rootUri.fsPath });
+				await vscode.window.showInformationMessage('Change has been reverted');
 			}
 		}),
 
